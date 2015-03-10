201e6bc1715a67349286ad9a940179c16d3a8655
James Harris <james.harris@icecave.com.au>
2015-03-10 11:23:27 +1000

    Added missing statuses.

--- src/Schedule/CompetitionStatus.php
+++ src/Schedule/CompetitionStatus.php
@@ -5,11 +5,15 @@ use Eloquent\Enumeration\AbstractEnumeration;
 
 class CompetitionStatus extends AbstractEnumeration
 {
-    const SCHEDULED   = 'scheduled';
-    const IN_PROGRESS = 'in-progress';
-    const DELAY_RAIN  = 'delay-rain';
-    const DELAY_OTHER = 'delay-other';
-    const SUSPENDED   = 'suspended';
-    const POSTPONED   = 'postponed';
-    const COMPLETE    = 'complete';
+    const SCHEDULED      = 'scheduled';
+    const IN_PROGRESS    = 'in-progress';
+    const DELAY_RAIN     = 'delay-rain';
+    const DELAY_OTHER    = 'delay-other';
+    const DELAY_DARKNESS = 'delay-darkness';
+    const SUSPENDED      = 'suspended';
+    const POSTPONED      = 'postponed';
+    const SHORTENED      = 'shortened';
+    const CANCELLED      = 'cancelled';
+    const OTHER          = 'other';
+    const COMPLETE       = 'complete';
 }
