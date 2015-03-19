b12b093d052f5d5c404beccaa64a5591f9cc25f9
James Harris <james.harris@icecave.com.au>
2015-03-20 09:37:49 +1000

    Added extra statuses to scope status, need to work out which ones are actually possible.

--- src/Score/ScopeStatus.php
+++ src/Score/ScopeStatus.php
@@ -5,6 +5,15 @@ use Eloquent\Enumeration\AbstractEnumeration;
 
 class ScopeStatus extends AbstractEnumeration
 {
-    const IN_PROGRESS = 'in-progress';
-    const COMPLETE    = 'complete';
+    const SCHEDULED      = 'scheduled';
+    const IN_PROGRESS    = 'in-progress';
+    const DELAY_RAIN     = 'delay-rain';
+    const DELAY_OTHER    = 'delay-other';
+    const DELAY_DARKNESS = 'delay-darkness';
+    const SUSPENDED      = 'suspended';
+    const POSTPONED      = 'postponed';
+    const SHORTENED      = 'shortened';
+    const CANCELLED      = 'cancelled';
+    const OTHER          = 'other';
+    const COMPLETE       = 'complete';
 }
