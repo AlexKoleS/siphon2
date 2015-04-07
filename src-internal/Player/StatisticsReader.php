7f8136809f4f792c16f09f97334fe7296ebfa0fb
James Harris <james.harris@icecave.com.au>
2015-04-07 10:35:23 +1000

    Implemented StatisticsReader atom methods.

--- src-internal/Player/StatisticsReader.php
+++ src-internal/Player/StatisticsReader.php
@@ -4,6 +4,7 @@ namespace Icecave\Siphon\Player;
 use Icecave\Siphon\Atom\AtomEntry;
 use Icecave\Siphon\Util;
 use Icecave\Siphon\XmlReaderInterface;
+use InvalidArgumentException;
 
 /**
  * Read data from player statistics feeds.
@@ -30,23 +31,12 @@ class StatisticsReader implements PlayerReaderInterface
      */
     public function read($sport, $league, $season, $teamId)
     {
-        $sport  = strtolower($sport);
-        $league = strtoupper($league);
-
-        $xml = $this
-            ->xmlReader
-            ->read(
-                sprintf(
-                    '/sport/v2/%s/%s/player-stats/%s/player_stats_%d_%s.xml',
-                    $sport,
-                    $league,
-                    $season,
-                    Util::extractNumericId($teamId),
-                    $league
-                )
-            );
-
-        return $this->factory->create($xml);
+        return $this->readImpl(
+            $sport,
+            $league,
+            $season,
+            Util::extractNumericId($teamId)
+        );
     }
 
     /**
@@ -58,9 +48,18 @@ class StatisticsReader implements PlayerReaderInterface
      */
     public function readAtomEntry(AtomEntry $atomEntry)
     {
-        throw new InvalidArgumentException(
-            'Unsupported atom entry.'
+        if (!$this->supportsAtomEntry($atomEntry)) {
+            throw new InvalidArgumentException(
+                'Unsupported atom entry.'
+            );
+        }
+
+        list($sport, $league, $season, $teamId) = Util::parse(
+            self::URL_PATTERN,
+            $atomEntry->resource()
         );
+
+        return $this->readImpl($sport, $league, $season, $teamId);
     }
 
     /**
@@ -72,8 +71,48 @@ class StatisticsReader implements PlayerReaderInterface
      */
     public function supportsAtomEntry(AtomEntry $atomEntry)
     {
-        return false;
+        if ($atomEntry->parameters()) {
+            return false;
+        }
+
+        return null !== Util::parse(
+            self::URL_PATTERN,
+            $atomEntry->resource()
+        );
     }
+
+    /**
+     * Read a player statistics feed.
+     *
+     * @param string $sport  The sport (eg, baseball, football, etc)
+     * @param string $league The league (eg, MLB, NFL, etc)
+     * @param string $season The season name.
+     * @param string $teamId The ID of the team.
+     *
+     * @return array<StatisticsInterface>
+     */
+    private function readImpl($sport, $league, $season, $teamId)
+    {
+        $sport  = strtolower($sport);
+        $league = strtoupper($league);
+
+        $xml = $this
+            ->xmlReader
+            ->read(
+                sprintf(
+                    self::URL_PATTERN,
+                    $sport,
+                    $league,
+                    $season,
+                    $teamId,
+                    $league
+                )
+            );
+
+        return $this->factory->create($xml);
+    }
+
+    const URL_PATTERN = '/sport/v2/%s/%s/player-stats/%s/player_stats_%d_%s.xml';
 
     private $xmlReader;
     private $statisticsFactory;
