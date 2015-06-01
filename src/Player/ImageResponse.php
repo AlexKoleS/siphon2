bfc8b01a38d721da2a15cfb214c53901e76c22ee
James Harris <james.harris@icecave.com.au>
2015-06-01 12:26:59 +1000

    Moved common implementation of player responses into PlayerResponseTrait.

--- src/Player/ImageResponse.php
+++ src/Player/ImageResponse.php
@@ -4,9 +4,6 @@ namespace Icecave\Siphon\Player;
 use Countable;
 use Icecave\Siphon\Reader\ResponseInterface;
 use Icecave\Siphon\Reader\ResponseVisitorInterface;
-use Icecave\Siphon\Schedule\Season;
-use Icecave\Siphon\Sport;
-use Icecave\Siphon\Team\TeamInterface;
 use IteratorAggregate;
 
 /**
@@ -17,109 +14,7 @@ class ImageResponse implements
     Countable,
     IteratorAggregate
 {
-    public function __construct(
-        Sport $sport,
-        Season $season,
-        TeamInterface $team
-    ) {
-        $this->setSport($sport);
-        $this->setSeason($season);
-        $this->setTeam($team);
-
-        $this->players = [];
-    }
-
-    /**
-     * Get the requested sport.
-     *
-     * @return Sport The requested sport.
-     */
-    public function sport()
-    {
-        return $this->sport;
-    }
-
-    /**
-     * Set the requested sport.
-     *
-     * @param Sport $sport The requested sport.
-     */
-    public function setSport(Sport $sport)
-    {
-        $this->sport = $sport;
-    }
-
-    /**
-     * Get the requested season.
-     *
-     * @return Season The requested season.
-     */
-    public function season()
-    {
-        return $this->season;
-    }
-
-    /**
-     * Set the requested season.
-     *
-     * @param Season $season The requested season.
-     */
-    public function setSeason(Season $season)
-    {
-        $this->season = $season;
-    }
-
-    /**
-     * Get the requested team.
-     *
-     * @return TeamInterface The requested team.
-     */
-    public function team()
-    {
-        return $this->team;
-    }
-
-    /**
-     * Set the requested team.
-     *
-     * @param TeamInterface $team The requested team.
-     */
-    public function setTeam(TeamInterface $team)
-    {
-        $this->team = $team;
-    }
-
-    /**
-     * Check if the response contains players.
-     *
-     * @param boolean True if the response is empty; otherwise, false.
-     */
-    public function isEmpty()
-    {
-        return empty($this->players);
-    }
-
-    /**
-     * Get the number of players in the response.
-     *
-     * @return integer
-     */
-    public function count()
-    {
-        return count($this->players);
-    }
-
-    /**
-     * Iterate the players.
-     *
-     * @return mixed<tuple<Player, string, string>>
-     */
-    public function getIterator()
-    {
-        foreach ($this->players as $entry) {
-            yield $entry;
-        }
-    }
+    use PlayerResponseTrait;
 
     /**
      * Add a player to the response.
@@ -130,25 +25,7 @@ class ImageResponse implements
      */
     public function add(Player $player, $smallImage, $largeImage)
     {
-        $this->players[$player->id()] = [$player, $smallImage, $largeImage];
-    }
-
-    /**
-     * Remove a player from the response.
-     *
-     * @param Player $player The player to remove.
-     */
-    public function remove(Player $player)
-    {
-        unset($this->players[$player->id()]);
-    }
-
-    /**
-     * Remove all players from the response.
-     */
-    public function clear()
-    {
-        $this->players = [];
+        $this->entries[$player->id()] = [$player, $smallImage, $largeImage];
     }
 
     /**
@@ -162,9 +39,4 @@ class ImageResponse implements
     {
         return $visitor->visitImageResponse($this);
     }
-
-    private $sport;
-    private $type;
-    private $team;
-    private $players;
 }
---
+++ src/Player/PlayerResponseTrait.php
@@ -0,0 +1,139 @@
+<?php
+namespace Icecave\Siphon\Player;
+
+use Icecave\Siphon\Schedule\Season;
+use Icecave\Siphon\Sport;
+use Icecave\Siphon\Team\TeamInterface;
+
+/**
+ * Common implementation for response that operate per sport + season + team.
+ */
+trait PlayerResponseTrait
+{
+    public function __construct(
+        Sport $sport,
+        Season $season,
+        TeamInterface $team
+    ) {
+        $this->setSport($sport);
+        $this->setSeason($season);
+        $this->setTeam($team);
+
+        $this->entries = [];
+    }
+
+    /**
+     * Get the requested sport.
+     *
+     * @return Sport The requested sport.
+     */
+    public function sport()
+    {
+        return $this->sport;
+    }
+
+    /**
+     * Set the requested sport.
+     *
+     * @param Sport $sport The requested sport.
+     */
+    public function setSport(Sport $sport)
+    {
+        $this->sport = $sport;
+    }
+
+    /**
+     * Get the requested season.
+     *
+     * @return Season The requested season.
+     */
+    public function season()
+    {
+        return $this->season;
+    }
+
+    /**
+     * Set the requested season.
+     *
+     * @param Season $season The requested season.
+     */
+    public function setSeason(Season $season)
+    {
+        $this->season = $season;
+    }
+
+    /**
+     * Get the requested team.
+     *
+     * @return TeamInterface The requested team.
+     */
+    public function team()
+    {
+        return $this->team;
+    }
+
+    /**
+     * Set the requested team.
+     *
+     * @param TeamInterface $team The requested team.
+     */
+    public function setTeam(TeamInterface $team)
+    {
+        $this->team = $team;
+    }
+
+    /**
+     * Check if the response contains players.
+     *
+     * @param boolean True if the response is empty; otherwise, false.
+     */
+    public function isEmpty()
+    {
+        return empty($this->entries);
+    }
+
+    /**
+     * Get the number of players in the response.
+     *
+     * @return integer
+     */
+    public function count()
+    {
+        return count($this->entries);
+    }
+
+    /**
+     * Iterate the players.
+     *
+     * @return mixed<tuple<Player, SeasonDetails>>
+     */
+    public function getIterator()
+    {
+        foreach ($this->entries as $entry) {
+            yield $entry;
+        }
+    }
+
+    /**
+     * Remove a player from the response.
+     *
+     * @param Player $player The player to remove.
+     */
+    public function remove(Player $player)
+    {
+        unset($this->entries[$player->id()]);
+    }
+
+    /**
+     * Remove all players from the response.
+     */
+    public function clear()
+    {
+        $this->entries = [];
+    }
+
+    private $sport;
+    private $type;
+    private $team;
+    private $entries;
+}
