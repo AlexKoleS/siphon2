46a491e9f19cfdbd7320271db1fea913c5a221ba
James Harris <james.harris@icecave.com.au>
2015-06-05 12:59:27 +1000

    Fix docs [ci skip]

--- src/Player/PlayerRequestTrait.php
+++ src/Player/PlayerRequestTrait.php
@@ -81,7 +81,7 @@ trait PlayerRequestTrait
      * If a string ID is given it is validated against the sport, and the
      * numeric portion is extracted.
      *
-     * @param string|integer The string or numeric team ID.
+     * @param string|integer $teamId The string or numeric team ID.
      */
     public function setTeamId($teamId)
     {
